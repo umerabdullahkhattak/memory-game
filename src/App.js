3dae83a0edbb01602cf45aeaa6c657f167a8533b
umer <umer.a.khattak@gmail.com>
2023-04-11 04:38:24 +0500

    Added function to increment current score and total score, identify second click on the button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Picture from "./Picture";
+import Picture2  from "./Picture2";
 
 const App = () => {
-  // const [color, setColor] = useState("black");
-
-  // useEffect(() => {
-  //   const changeColorOnClick = () => {
-  //     if (color === "black") {
-  //       setColor("red");
-  //     } else {
-  //       setColor("black");
-  //     }
-  //   };
+  const [BestScore, SetBestScore] = useState(0);
+  const BestScoreIncrement = (score) => {
+    if ((score + BestScore) > BestScore) {
+      SetBestScore(BestScore + 1);
+    }
     
-  //   document.addEventListener("click", changeColorOnClick);
-
-  //   return () => {
-  //     document.removeEventListener("click", changeColorOnClick);
-  //   };
-  // }, [color]);
+  } 
 
   return (
     <div>
-      <Picture/>
+      <h1>Best Score: {BestScore}</h1>
+      <Picture BestScoreIncrement={BestScoreIncrement}/>
+      <Picture2 BestScoreIncrement={BestScoreIncrement}/>
+
     </div>
   );
 };
--- src/Picture2.js
+++ src/Picture2.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const Picture = (props) => {
+const Picture2 = (props) => {
   const [score, setScore] = useState(0);
   
 
@@ -13,7 +13,7 @@ const Picture = (props) => {
   };
 
   useEffect(() => {
-    const button = document.getElementById("myButton");
+    const button = document.getElementById("myButton2");
     button.addEventListener("click", scoreIncrement)
     return () => {
        
@@ -27,14 +27,14 @@ const Picture = (props) => {
         <h1>Score: {score}</h1>
         
       ) : (
-        <p>Button has been clicked second time</p>
+        <p>Button 2 has been clicked second time</p>
       )}
-      <button id="myButton">
-        Click me to increase counter, clicking me second time would reset the
+      <button id="myButton2">
+        Button 2 || Click me to increase counter, clicking me second time would reset the
         score
       </button>
     </div>
   );
 };
 
-export default Picture;
+export default Picture2;
